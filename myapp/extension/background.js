d1ce0462fb525d98bec7443f0ecf009ef147a6fc
alex bae <ajsbae@gmail.com>
2018-03-17 16:25:30 -0400

    made changes to original extension to track accurate time

--- myapp/extension/background.js
+++ myapp/extension/background.js
@@ -1,96 +1,59 @@
-
 //Arays that stores all the tracked domains
-let domains=[];
-
-// The current active domain
-let currentDomain={};
+let domains = [];
 
 
+let activeURL = null;
+let startTime = new Date();
 
-class Domain{
 
+function updateTime(url) {
+    if (!domains[url]) {
+        domains[url] = 0;
+    }
+    let deltaTime = new Date() - startTime;
+    // console.log(url);
+    // console.log(domains[url]);
+    domains[url] += deltaTime / 1000;
+}
 
-	constructor(url){
+function switchCurrentTab(url) {
+    if (activeURL != null) {
+        
+        updateTime(activeURL);
+    }
+    activeURL = url;
+    startTime = new Date();
+}
 
-		this.url=url;
-		this.startTime=0;
-		this.endTime;
-		this.timeElapsed=0;
-	}
-
-	updateTime(){
-		
-		this.startTime=this.endTime===undefined? new Date(): this.endTime;
-		this.endTime=new Date();
-		let timeintervals=this.endTime-this.startTime;
-		this.timeElapsed+=timeintervals;
-		console.log(this.url,this.timeElapsed);
-		
-		}
-
-	}
-
-let URL;
 
 //The indicator of setinterval(), used to stop the timer when the active tab is changed
 let current;
-
 // look up chrome.tabs documentation. Part of chrome extensions API
-chrome.tabs.onActivated.addListener((tab)=>{
+chrome.tabs.onActivated.addListener((tab) => {
 
-	//checks if desired domain is already stored in the domain array
-	let found=false;
-	
-	//every tab has a unique id for each browser session. look up extension API
-	let id=tab.tabId;
-	
+    //every tab has a unique id for each browser session. look up extension API
+    let id = tab.tabId;
 
-	// If no existing domain we create a new one other wise update the time spent on current domain
-	
-	chrome.tabs.get(id,(tab)=>{
-		
-		URL=tab.url;
-		
-		for(domain of domains){
-			if(domain.url===URL){
-				found=true;
-				currentDomain=domain;
-				break;
+    // If no existing domain we create a new one other wise update the time spent on current domain
+    chrome.tabs.get(id, (tab) => {
 
-			}
+            URL = tab.url;
+            console.log(URL);
+            switchCurrentTab(URL);
 
-		}
+            // let domainsObj = {};
+            // domainsObj["arrayKey"] = domains;
+
+            // console.log(domainsObj);
+            var newArray = JSON.parse(JSON.stringify(domains));
+            console.log(domains);
+            chrome.storage.sync.set({"arrayKey": domains }
+                ,
+                function() {
+                    console.log('Settings saved');
+                });
+
+    })
 
 
-	if(found){
-
-	//stops current timer
-		window.clearInterval(current);
-
-		if(currentDomain.timeElapsed!==0){
-
-				currentDomain.endTime=new Date()
-			}
-
-	//restarts timer with new active tab	
-		current=window.setInterval(function(){currentDomain.updateTime();},1000)
-		
-	}
-
-	
-	if(!found){
-
-		window.clearInterval(current);
-		let site= new Domain(URL);
-		domains.push(site);
-		currentDomain=site;
-		current=window.setInterval(function(){currentDomain.updateTime();},1000)
-		
-
-	}
-
-	})
-
-		
-})	
-
+})
