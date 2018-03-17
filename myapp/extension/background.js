bf11d40f660f33d72965911534e78145ec78c80b
ih646 <ih646@nyu.edu>
2018-03-17 03:06:01 -0400

    fixed timer issues. Can now track time with reasonable accuracy

--- myapp/extension/background.js
+++ myapp/extension/background.js
@@ -2,8 +2,8 @@
 //Arays that stores all the tracked domains
 let domains=[];
 
-// The current active doamin
-let currentDomain;
+// The current active domain
+let currentDomain={};
 
 
 
@@ -20,80 +20,77 @@ class Domain{
 
 	updateTime(){
 		
-		// The conditional resets the timer whenever we switch back to a tab that was previously active
-		if(this.timeElapsed!==0){
-
-			this.endTime=undefined;
-			console.log(this.endTime);
-		}
+		this.startTime=this.endTime===undefined? new Date(): this.endTime;
+		this.endTime=new Date();
+		let timeintervals=this.endTime-this.startTime;
+		this.timeElapsed+=timeintervals;
+		console.log(this.url,this.timeElapsed);
 		
-		//looke up documentation. basically calls a function over and over again after a set time interval.
-		window.setInterval(()=>{
-
-				this.startTime=this.endTime===undefined?new Date(): this.endTime;
-				this.endTime=new Date();
-				let timeintervals=this.endTime-this.startTime;
-				this.timeElapsed+=timeintervals;
-
-			},1000);
-			
 		}
 
 	}
 
+let URL;
+
+//The indicator of setinterval(), used to stop the timer when the active tab is changed
+let current;
 
 // look up chrome.tabs documentation. Part of chrome extensions API
 chrome.tabs.onActivated.addListener((tab)=>{
 
-	id=tab.tabId;
-	let found=false; //checks if desired domain is already stored in the domain array
+	//checks if desired domain is already stored in the domain array
+	let found=false;
+	
+	//every tab has a unique id for each browser session. look up extension API
+	let id=tab.tabId;
+	
 
 	// If no existing domain we create a new one other wise update the time spent on current domain
 	
 	chrome.tabs.get(id,(tab)=>{
-
-		domains.forEach((domain)=>{
-
-			if(domain.url===tab.url){
-
+		
+		URL=tab.url;
+		
+		for(domain of domains){
+			if(domain.url===URL){
 				found=true;
 				currentDomain=domain;
+				break;
 
 			}
 
+		}
 
-		})
 
-	
 	if(found){
 
-		currentDomain.updateTime();
-		
-		window.setInterval(function(){
-					
-				console.log(currentDomain.url,currentDomain.timeElapsed);
-			
-			},1000)
+	//stops current timer
+		window.clearInterval(current);
 
+		if(currentDomain.timeElapsed!==0){
+
+				currentDomain.endTime=new Date()
+			}
+
+	//restarts timer with new active tab	
+		current=window.setInterval(function(){currentDomain.updateTime();},1000)
+		
 	}
 
 	
-	else{
+	if(!found){
 
-		let site=new Domain(tab.url);
-		currentDomain=site;
-		console.log(site);
+		window.clearInterval(current);
+		let site= new Domain(URL);
 		domains.push(site);
-		currentDomain.updateTime();
-		window.setInterval(function(){
-					
-				console.log(currentDomain.url,currentDomain.timeElapsed);
-			
-			},1000)
+		currentDomain=site;
+		current=window.setInterval(function(){currentDomain.updateTime();},1000)
+		
 
 	}
-		
+
 	})
 
-})
-	
+		
+})	
+
