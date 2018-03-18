acf4418df8384797b21805771cfb845253612f64
Zachary Kimelheim <>
2018-03-18 16:46:00 -0400

    signup page added with bug

--- myapp/db.js
+++ myapp/db.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
 mongoose.connect('mongodb://admin:admin@ds249428.mlab.com:49428/ott', (err, db) => {
     if (err) {
         return console.log(err);
@@ -6,19 +9,31 @@ mongoose.connect('mongodb://admin:admin@ds249428.mlab.com:49428/ott', (err, db) 
 });
 
 
-var Name = new mongoose.Schema({
+var User = new mongoose.Schema({
     first: String,
     last: String,
+    username: String,
+    password: String,
+    admin: Boolean,
 
 });
 
 
 
-mongoose.model('Name', Name);
+mongoose.model('User', User);
 
 
-module.exports = mongoose.model('Name');
-module.exports = {
-    Name: Name,
+module.exports = mongoose.model('User');
+module.exports.User = User;
 
+// Passport functions used for authentication
+module.exports.createUser = function (newUser, callback) {
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(newUser.password, salt, function (err, hash) {
+            newUser.password = hash
+            newUser.save(callback)
+        })
+    })
 };
+
+
