ce9af05ebdf297664623fa8e31762c068e188a6f
ih646 <ih646@nyu.edu>
2018-04-26 02:19:32 -0400

    finish all functionality of project

--- myapp/app.js
+++ myapp/app.js
@@ -6,11 +6,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
-
-
-
 var index = require('./routes/index');
 var auth = require('./routes/auth');
 var app = express();
